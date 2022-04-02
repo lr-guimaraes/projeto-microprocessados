5a0affc4cac0abc9bd8b11843e49613d1d67ac2a
lr-guimaraes <leandrorg307@gmail.com>
2022-04-02 17:48:18 -0300

    update

--- README.md
+++ README.md
@@ -1,1 +1,22 @@
 # projeto-microprocessados
+
+# Commands
+
+
+- set bit  
+
+- cbi - clear bit  
+
+- jmp e rjmp - pule para o endereço de memoria  
+
+- sbis - pula a instrução se bit is set  
+
+- sbic - pula a instrução se bit is clear 
+
+- DDR_: INDICE DIREÇÃO DO BIT DE PORTA { 0: ENTRADA; 1: SAIDA } 
+
+- PORT_: SAIDA DO BIT DE UMA PORTA 
+
+- PIN_: LEITURA DE UM BIT DE UMA PORTA 
+
+ 
---
+++ s1 nor (s2 xor s3).asm
@@ -0,0 +1,44 @@
+; S1 nor (S2 xorS3)
+start:
+    cbi DDRD,3
+	cbi DDRD,4
+	cbi DDRD,5
+
+test_PD3:
+	;if PD3 == 0
+	sbis DDRD,3 
+	rjmp test1_PD4
+	rjmp test2_PD4
+
+	sbic DDRD,3
+	rjmp turns_on
+
+test1_PD4: ; 
+
+	sbis DDRD,4  ;if ==1
+	rjmp test1_PD5	
+	; if PD3 and PD4 are equal to 	
+	sbis DDRD,4
+	rjmp turns_on
+
+test2_PD4:
+	sbic DDRD,4  ;if == 1
+	rjmp test2_PD5	
+	; if PD3 and PD4 are equal to 	
+	sbis DDRD,4
+	rjmp turns_on
+
+test1_PD5:
+	sbis DDRD,5 
+	rjmp turns_on
+	rjmp turns_off
+
+test2_PD5:
+	sbic DDRD,5 
+	rjmp turns_off
+	rjmp turns_on
+
+turns_on:
+	sbi PORTD,6
+turns_off:
+	cbi PORTD,6
