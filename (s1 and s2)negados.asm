a6e639816994ed5d35fa246a360ee37cf121f5e2
Leandro Ricardo Guimarães <84518335+lr-guimaraes@users.noreply.github.com>
2022-04-05 09:17:07 -0300

    Update (s1 and s2)negados.asm

--- (s1 and s2)negados.asm
+++ (s1 and s2)negados.asm
@@ -1,13 +1,13 @@
 ;Defines IO
 ;function: ~S1 and ~S2
 start:
-	cbi DDRD,3 ;PD3
-	cbi DDRD,4 ;PD4
+	cbi DDRD,3      ;PD3
+	cbi DDRD,4      ;PD4
 
-test_PD4:
+test_PD3:
 
-	sbic DDRD,4		
-	rjmp test_PD3  ;if DDRD4 == 0, test PD3, else turns 
+	sbic DDRD,3		
+	rjmp test_PD3  ;if DDRD4 == 0, test PD4, else turns 
 	rjmp turns_off
 
 turns_on:
@@ -16,9 +16,9 @@ turns_on:
 turns_of
 	cbi PORTD,5 
 
-test_PD3:
+test_PD4:
 
-	sbic DDRD,3
+	sbic DDRD,4
 	rjmp turns_on
 	rjmp turns_off
 
