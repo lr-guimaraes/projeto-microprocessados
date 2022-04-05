d19bc2478f6cd0c4163ce0f4f2333a54f92c79f3
Leandro Ricardo Guimarães <84518335+lr-guimaraes@users.noreply.github.com>
2022-04-05 09:27:34 -0300

    Update s1 nor (s2 xor s3).asm

--- s1 nor (s2 xor s3).asm
+++ s1 nor (s2 xor s3).asm
@@ -1,44 +1,44 @@
 ; S1 nor (S2 xorS3)
 start:
+    cbi DDRD,2
     cbi DDRD,3
-	cbi DDRD,4
-	cbi DDRD,5
+    cbi DDRD,4
 
-test_PD5:
-	;if PD3 == 0
-	sbis DDRD,3 
-	rjmp test1_PD4
-	rjmp test2_PD4
+test_PD4:
+	;if PD4 == 0
+	sbis DDRD,4 
+	rjmp test1_PD3
+	rjmp test2_PD3
 
+	sbic DDRD,2
+	rjmp turns_on
+
+test1_PD3: ; 
+
+	sbis DDRD,3  ;if ==1
+	rjmp test1_PD2	
+	; if PD3 and PD4 are equal to 	
 	sbic DDRD,3
 	rjmp turns_on
 
-test1_PD4: ; 
-
-	sbis DDRD,4  ;if ==1
-	rjmp test1_PD3	
-	; if PD3 and PD4 are equal to 	
-	sbic DDRD,4
-	rjmp turns_on
-
-test2_PD4:
-	sbic DDRD,4  ;if == 1
-	rjmp test2_PD3	
-	; if PD3 and PD4 are equal to 	
-	sbis DDRD,4
-	rjmp turns_on
-
-test1_PD3:
+test2_PD3:
+	sbic DDRD,3  ;if == 1
+	rjmp test2_PD2	
+	; if PD2 and PD3 are equal to 	
 	sbis DDRD,3
+	rjmp turns_on
+
+test1_PD2:
+	sbis DDRD,2
 	rjmp turns_on
 	rjmp turns_off
 
-test2_PD3:
-	sbic DDRD,3 
+test2_PD2:
+	sbic DDRD,2 
 	rjmp turns_off
 	rjmp turns_on
 
 turns_on:
-	sbi PORTD,6
+	sbi PORTD,5
 turns_off:
-	cbi PORTD,6
+	cbi PORTD,5
