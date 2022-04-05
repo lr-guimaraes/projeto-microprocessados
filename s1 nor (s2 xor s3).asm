9e4c5ccd6a1ed14ceea79b5aeae9dfb64e33ac6f
Leandro Ricardo Guimarães <84518335+lr-guimaraes@users.noreply.github.com>
2022-04-05 09:18:17 -0300

    Update s1 nor (s2 xor s3).asm

--- s1 nor (s2 xor s3).asm
+++ s1 nor (s2 xor s3).asm
@@ -4,7 +4,7 @@ start:
 	cbi DDRD,4
 	cbi DDRD,5
 
-test_PD3:
+test_PD5:
 	;if PD3 == 0
 	sbis DDRD,3 
 	rjmp test1_PD4
@@ -16,25 +16,25 @@ test_PD3:
 test1_PD4: ; 
 
 	sbis DDRD,4  ;if ==1
-	rjmp test1_PD5	
+	rjmp test1_PD3	
 	; if PD3 and PD4 are equal to 	
 	sbic DDRD,4
 	rjmp turns_on
 
 test2_PD4:
 	sbic DDRD,4  ;if == 1
-	rjmp test2_PD5	
+	rjmp test2_PD3	
 	; if PD3 and PD4 are equal to 	
 	sbis DDRD,4
 	rjmp turns_on
 
-test1_PD5:
-	sbis DDRD,5 
+test1_PD3:
+	sbis DDRD,3
 	rjmp turns_on
 	rjmp turns_off
 
-test2_PD5:
-	sbic DDRD,5 
+test2_PD3:
+	sbic DDRD,3 
 	rjmp turns_off
 	rjmp turns_on
 
